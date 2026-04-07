71169c0d2286cb5d17fba5cc63eb80ef1c50013d
Arif14377 <arifrahman14377@gmail.com>
2026-04-07 11:08:49 +0700

    feat: create users model

---
+++ backend/internal/models/users.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Users struct {
+	Id         uuid.UUID `json:"id"`
+	Email      string    `json:"email"`
+	Password   string    `json:"password"`
+	Created_At time.Time `json:"created_at"`
+}
+
+// DTO
+type AuthUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
