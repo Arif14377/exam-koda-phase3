a38c5f91a058403e741653d908e8fe2633bc14a6
Arif14377 <arifrahman14377@gmail.com>
2026-04-07 17:16:12 +0700

    feat: create auth middleware

---
+++ backend/internal/middleware/auth.go
@@ -0,0 +1,71 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/Arif14377/exam-koda-phase3/internal/models"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// ambil token dari header
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, models.Response{
+				Success: false,
+				Message: "Authorization required.",
+			})
+			c.Abort()
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, models.Response{
+				Success: false,
+				Message: "invalid token format.",
+			})
+		}
+
+		// validasi token
+		tokenString := parts[1]
+		secret := os.Getenv("JWT_SECRET")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+			return []byte(secret), nil
+		})
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, models.Response{
+				Success: false,
+				Message: "Token is invalid or expired.",
+			})
+			c.Abort()
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, models.Response{
+				Success: false,
+				Message: "Failed to process claims token.",
+			})
+			c.Abort()
+			return
+		}
+
+		userId := ""
+		if val, exists := claims["id"]; exists {
+			userId = val.(string)
+		}
+
+		// next
+		c.Set("userId", userId)
+		c.Set("userEmail", claims["email"])
+		c.Next()
+
+	}
+}
